e2a1deeae47509db53f42dc6ed5fa8a6df49b153
efeaydemir5 <43586787+efeaydemir5@users.noreply.github.com>
2018-09-26 19:22:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,7 +147,7 @@ if (message.content === prefix + "kurabiye") {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
+		if (!msg.guild.member(msg.author).hasPermission) {
 			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
 		} else {
 		msg.reply('Aleyküm selam, hoş geldin ^^');
